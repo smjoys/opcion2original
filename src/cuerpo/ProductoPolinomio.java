d68cdced6a0bee0be5e94c17bc8e749d84706f79
Sergio Lara <chechopro.2002@gmail.com>
2022-04-21 15:13:09 -0400

    ultimo push xd 2

--- src/cuerpo/ProductoPolinomio.java
+++ src/cuerpo/ProductoPolinomio.java
@@ -4,7 +4,7 @@ public class ProductoPolinomio {
     PolinomioList Polinomio1 = new PolinomioList();
     PolinomioList Polinomio2 = new PolinomioList();
 
-    int Ntermino;
+    int Nterm1;
     int Nterm2;
 
     public ProductoPolinomio(Nodo Poli1, Nodo Poli2, int Nterm1, int Nterm2){
